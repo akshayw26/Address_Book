5f641a94e18a2606540a97bb78d1bcf66dbdcf84
Akshayw26 <wangade30@gmail.com>
2022-08-17 15:17:36 +0530

    Added - Add New Person

--- src/com/AddressBook/AddressBookMain.java
+++ src/com/AddressBook/AddressBookMain.java
@@ -3,11 +3,15 @@ package com.AddressBook;
 import java.util.Scanner;
 
 public class AddressBookMain {
-
+    Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-
         System.out.println("welcome to Address Book Program");
         Scanner scan = new Scanner(System.in);
+        AddressBookMain abm = new AddressBookMain();
+        abm.personDetails();
+        System.out.println();
+    }
+    void personDetails(){
         System.out.println("Enter first and last name --> ");
         String firstName = scan.next();
         String lastName = scan.next();
@@ -29,5 +33,16 @@ public class AddressBookMain {
         System.out.println("city --> " +city);
         System.out.println("zip code --> " +zip);
         System.out.println("Phone Number --> " +phoneNumber);
+
+        System.out.println("Enter 1 to Add Details of new person");
+        System.out.println("Enter 2 to exit");
+        int choice = scan.nextInt();
+        switch (choice){
+            case 1:
+                personDetails();
+                break;
+            case 2:
+                System.out.println("Exited From Address book");
+        }
     }
 }
